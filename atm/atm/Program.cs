d7207c17b066b823e04f74f0edf634945e369ac4
jcqnly <jcqnly@gmail.com>
2018-06-06 17:41:57 -0700

    add deposit method

--- atm/atm/Program.cs
+++ atm/atm/Program.cs
@@ -4,7 +4,15 @@ namespace atm
 {
     class Program
     {
+        public static double Balance = 5000;
+
         static void Main(string[] args)
+        {
+            Options();
+            Console.Read();
+        }
+
+        public static void Options()
         {
             Console.WriteLine("Welcome to the bestest ATM!\n" +
                 "What would you like to do?\n" +
@@ -13,7 +21,46 @@ namespace atm
                 "3. View Balance\n" +
                 "4. Exit\n");
 
-            Console.Read();
+            int selection = int.Parse(Console.ReadLine());
+
+            UserSelection(selection);
+            Console.WriteLine($"Balance is now {Balance}");
         }
+
+        public static void UserSelection(int selection)
+        {
+           switch (selection)
+           {
+               case 1:
+                   Deposit();
+               break;
+
+               case 2:
+                    Withdraw();
+               break;
+
+               case 3:
+
+               break;
+
+               case 4:
+                   Environment.Exit(0);
+                   break;
+
+               default:
+                    Console.WriteLine("Not valid.  Select again");
+                    Options();
+                    break;
+           }
+        }
+
+        public static double Deposit()
+        {
+            Console.WriteLine("How much would you like to deposit?\n");
+            double userDeposit = double.Parse(Console.ReadLine());
+            Balance += userDeposit;
+            return Balance;
+        }
+
     }
 }
